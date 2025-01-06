8eaaefe360802ca26b438093e9f8e508934ec4c0
RowlaxX <32131492+RowlaxX@users.noreply.github.com>
2025-01-06 10:49:49 +0100

    Carreeeeee

--- V2/pattern_finder.py
+++ V2/pattern_finder.py
@@ -2,10 +2,50 @@ import mine_field as mf
 import numpy as np
 from sklearn.cluster import DBSCAN
 
-def find_pattern(field: mf.MineField):
+import numpy as np
+
+def _find_densest_zone_1D(values, zone_size) -> int:
+    start = min(values)
+    end = max(values)
+    hist = [0] * (end - start + 1)
+
+    if len(hist) < zone_size:
+        blank = zone_size - len(hist)
+        return start - blank // 2
+
+    for v in values:
+        hist[v - start] += 1
+
+    m = 0
+    i = -1
+
+    for j in range(len(hist) - zone_size):
+        s = sum(hist[j:j+zone_size])
+
+        if s > m:
+            m = s
+            i = j
+
+    return start + i
+    
+
+def _find_densest_zone(points, zone_size) -> tuple[int, int]:
+    x = _find_densest_zone_1D(tuple(p[0] for p in points), zone_size)
+    y = _find_densest_zone_1D(tuple(p[1] for p in points), zone_size)
+    return (x, y)
+
+def find_pattern(field: mf.MineField) -> list[tuple[int, int]]:
     data = np.array([(p.x, p.y) for p in field.points()])
     eps = field.pattern_size / 2.5
     dbscan = DBSCAN(eps=eps, min_samples=7)
     labels = dbscan.fit_predict(data)
-    return labels
 
+    clusters = [[] for _ in range(max(labels) + 1)]
+
+    for label, point in zip(labels, data):
+        if label == -1:
+            continue
+
+        clusters[label].append(point)
+    
+    return [_find_densest_zone(cluster, field.pattern_size) for cluster in clusters]
--- V2/pattern_finder_test.py
+++ V2/pattern_finder_test.py
@@ -5,9 +5,9 @@ import mine_field_drawer as mfd
 
 from PIL import ImageDraw
 
-field = mfg.gen_mine_field(pattern_count=6)
+p_size = 20
+field = mfg.gen_mine_field(pattern_count=6, pattern_size=p_size)
 image = mfd.to_image(field)
-labels = pf.find_pattern(field)
 draw = ImageDraw.Draw(image)
 
 for pattern in field.patterns:
@@ -15,18 +15,9 @@ for pattern in field.patterns:
     xy2 = (xy1[0] + pattern.pattern_size + 2, xy1[1] + pattern.pattern_size + 2)
     draw.rectangle(xy=(xy1, xy2), outline='green')
 
-xys = [[(field.max_x, field.max_y),(0, 0)] for _ in range(max(labels) + 1)]
-
-for label, point in zip(labels, field.points()):
-    if label == -1:
-        continue
-
-    xy = xys[label]
-    xy[0] = (min(point.x, xy[0][0]), min(point.y, xy[0][1]))
-    xy[1] = (max(point.x, xy[1][0]), max(point.y, xy[1][1]))
-
-for xy in xys:
-    print(xy)
-    draw.rectangle(xy=xy, outline='red')
+for xy1 in pf.find_pattern(field):
+    xy1 = (xy1[0] - 1, xy1[1] - 1)
+    xy2 = (xy1[0] + p_size + 2, xy1[1] + p_size + 2)
+    draw.rectangle(xy=(xy1, xy2), outline='red')
 
 image.save('V2/test_result/pattern_finder_test.png')
